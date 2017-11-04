ab20a843ccdbc2d7028de16dc8a5c66bb768d31d
Jeff Kloosterman <jkloosterman@michiganlabs.com>
2017-11-04 19:43:36 -0400

    Add map to event detail

--- AvenueForTheArts/UI/Events/EventDetailController.swift
+++ AvenueForTheArts/UI/Events/EventDetailController.swift
@@ -1,11 +1,13 @@
 import UIKit
 import Alamofire
 import AlamofireImage
+import MapKit
 
 class EventDetailController: UIViewController {
     @IBOutlet private weak var name: UILabel!
     @IBOutlet private weak var descriptionView: UITextView!
     @IBOutlet private weak var banner: UIImageView!
+    @IBOutlet private weak var mapView: MKMapView!
 
     var event: API.Models.Event!
 
@@ -19,6 +21,39 @@ class EventDetailController: UIViewController {
             self.banner.af_setImage(withURL: url)
         } else {
             self.banner.isHidden = true
+        }
+
+        let span = MKCoordinateSpan(latitudeDelta: 0.015, longitudeDelta: 0.015)
+        if let lat = self.event.place.location?.latitude, let long = self.event.place.location?.longitude {
+            let location = CLLocation(latitude: Double(lat), longitude: Double(long))
+
+            CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (placemarks, error) in
+                guard error == nil, let center = placemarks?.first?.location else {
+                    self.mapView.isHidden = true
+                    return
+                }
+
+                placemarks?.forEach {
+                    self.mapView.addAnnotation(MKPlacemark(placemark: $0))
+                }
+                let region = MKCoordinateRegion(center: center.coordinate, span: span)
+                self.mapView.setRegion(region, animated: true)
+            })
+
+        } else {
+            CLGeocoder().geocodeAddressString(self.event.place.name, completionHandler: { (placemarks, error) in
+                guard error == nil, let center = placemarks?.first?.location else {
+                    self.mapView.isHidden = true
+                    return
+                }
+
+                placemarks?.forEach {
+                    self.mapView.addAnnotation(MKPlacemark(placemark: $0))
+                }
+
+                let region = MKCoordinateRegion(center: center.coordinate, span: span)
+                self.mapView.setRegion(region, animated: true)
+            })
         }
     }
 }
