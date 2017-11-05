1a99585246ff9fd168cbb0388d3f68bf2abf156b
Chris Carr <ccarr@michiganlabs.com>
2017-11-05 14:11:08 -0500

    Add Info screen and attempt to add TabBar items

---
+++ AvenueForTheArts/UI/Info/InfoController.swift
@@ -0,0 +1,13 @@
+//
+//  InfoController.swift
+//  AvenueForTheArts
+//
+//  Created by Chris Carr on 11/5/17.
+//  Copyright © 2017 MichiganLabs. All rights reserved.
+//
+
+import UIKit
+
+class InfoController: UIViewController {
+    // stuff
+}
