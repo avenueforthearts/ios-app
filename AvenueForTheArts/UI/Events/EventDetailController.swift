87834093a467cd719e79966fddffee9bcb16958b
Jeff Kloosterman <jkloosterman@michiganlabs.com>
2017-11-04 23:07:50 -0400

    Restyle event details

--- AvenueForTheArts/UI/Events/EventDetailController.swift
+++ AvenueForTheArts/UI/Events/EventDetailController.swift
@@ -4,15 +4,27 @@ import AlamofireImage
 import MapKit
 
 class EventDetailController: UIViewController {
+    @IBOutlet private weak var scrollView: UIScrollView!
     @IBOutlet private weak var name: UILabel!
     @IBOutlet private weak var descriptionView: UITextView!
     @IBOutlet private weak var banner: UIImageView!
     @IBOutlet private weak var mapView: MKMapView!
+    @IBOutlet private weak var bannerTop: NSLayoutConstraint!
+    @IBOutlet private weak var bannerAspect: NSLayoutConstraint!
+    @IBOutlet private weak var statusBarMask: UIView!
+    @IBOutlet private weak var statusBarMaskHeight: NSLayoutConstraint!
 
     var event: API.Models.Event!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.statusBarMask.alpha = 0
+        self.statusBarMask.layer.shadowRadius = 4
+        self.statusBarMask.layer.shadowColor = UIColor.darkGray.cgColor
+        self.statusBarMask.layer.shadowOpacity = 1
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = true
+        self.navigationController?.interactivePopGestureRecognizer?.delegate = self
 
         self.name.text = self.event.name
         self.descriptionView.text = self.event.description
@@ -37,7 +49,7 @@ class EventDetailController: UIViewController {
                     self.mapView.addAnnotation(MKPlacemark(placemark: $0))
                 }
                 let region = MKCoordinateRegion(center: center.coordinate, span: span)
-                self.mapView.setRegion(region, animated: true)
+                self.mapView.setRegion(region, animated: false)
             })
 
         } else {
@@ -52,8 +64,35 @@ class EventDetailController: UIViewController {
                 }
 
                 let region = MKCoordinateRegion(center: center.coordinate, span: span)
-                self.mapView.setRegion(region, animated: true)
+                self.mapView.setRegion(region, animated: false)
             })
+        }
+    }
+
+    @available(iOS 11.0, *)
+    override func viewSafeAreaInsetsDidChange() {
+        super.viewSafeAreaInsetsDidChange()
+        self.statusBarMaskHeight.constant = self.view.safeAreaInsets.top
+    }
+}
+
+extension EventDetailController: UIGestureRecognizerDelegate {
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+}
+
+extension EventDetailController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let y = scrollView.contentOffset.y
+        let imageHeight = self.banner.frame.height
+        self.statusBarMask.alpha = min(1, max(0, y / imageHeight))
+        if y < 0 {
+            self.bannerTop.constant = 0
+            self.bannerAspect.constant = 1.5 * y
+        } else {
+            self.bannerAspect.constant = 0
+            self.bannerTop.constant = -0.2 * y
         }
     }
 }
